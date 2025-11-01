cad82224208952669e6a5af666508336f991578a
Daniel Jacob Butcher <daniel@quokka.wiki>
2025-11-01 16:57:21 +0200

    Added dark favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import faviconDark from '$lib/assets/favicon-dark.svg';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<link rel="icon" href={faviconDark} media="(prefers-color-scheme: dark)">
 </svelte:head>
 
 {@render children()}
